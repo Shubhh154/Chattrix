2fa56c279c4fe7b61d280e80720ee4711a064ffa
Shubhh154 <shubhamsherkhane2019@gmail.com>
2025-07-04 00:20:28 +0530

    Chattrix v0.2

--- backend/src/index.js
+++ backend/src/index.js
@@ -63,6 +63,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
+    // origin: process.env.CLIENT_URL || "http://localhost:5173",
     origin: "http://localhost:5173",
     credentials: true,
   })
@@ -78,7 +79,7 @@ try {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
