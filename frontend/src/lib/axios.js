aaec15b576ee5db279a7a51e0fd8cddd9f45371e
Shubham Sherkhane <174640806+Shubhh154@users.noreply.github.com>
2025-10-02 10:10:00 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  //baseURL: "https://chattrix-backend-tdbp.onrender.com",
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+  baseURL: "https://chattrix-backend-tdbp.onrender.com",
   withCredentials: true,
 });
