6075df0443e9506545f8105b662b73bcfeca3376
Shubhh154 <shubhamsherkhane2019@gmail.com>
2025-10-02 09:58:49 +0530

    backend -> index.js Updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -79,7 +79,7 @@ try {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
